c421e0a2ffa56f74efd7a3334052a3928592cddd
Timofffee <timofffee@gmail.com>
2025-09-11 16:50:58 +0400

    Update nodebase

--- 02_nodebase/server.js
+++ 02_nodebase/server.js
@@ -1,17 +1,30 @@
 import express from "express";
-import path from "path";
-import { fileURLToPath } from "url";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 const app = express();
 const PORT = 5555;
 
-// app.use(express.static(path.join(__dirname, "public")));
+app.use(express.json());
+app.use(express.static("./public"));
 
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "public/home.html"));
+let tasks = [
+    { id: 1, title: "Купить хлеб" },
+    { id: 2, title: "Сделать домашку" },
+];
+
+app.get("/api/tasks", (req, res) => {
+    res.json(tasks);
+});
+
+app.post("/api/tasks", (req, res) => {
+    const task = { id: tasks.length + 1, title: req.body.taskTitle };
+    tasks.push(task);
+    res.status(201).json(tasks);
+});
+
+app.delete("/api/tasks/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+    tasks = tasks.filter((t) => t.id !== id);
+    res.sendStatus(204);
 });
 
 app.listen(PORT, () => {
